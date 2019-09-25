76beec4f0b5da842cefc9a99cff7e13eda4770be
Daniel Moreira <dmcardoso.99@hotmail.com>
2019-09-25 13:53:07 -0300

    remove moment do index e remove comentários da callback de listen da aplciação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,8 @@
 const app = require('express')();
-const moment = require('moment');
-
-moment.locale('pt-BR');
 
 require('./app/middlewares/index')(app);
 require('./app/api/index')(app);
 
 app.listen(3032, () => {
     console.log('feedtrack-backend listening on 3032!');
-    // console.log(;
-    // console.log(new Date());
 });
